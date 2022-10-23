b2f3c6e447913e81386e0615c7b279d3e4d5aa18
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-10-23 23:20:13 +0300

    still figuring out the design

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+import img1 from '../../assets/stars.png';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <h1>IMAGINE A PLACE...</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className='navbar-button'>ff</div>
+        <div className='navbar-button'>Test</div>
+    </div>
+  )
+}
+
+export default Navbar
