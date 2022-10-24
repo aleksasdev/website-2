7d386c88b56dbc31678d551e3dc74efdff47b7ae
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-10-24 22:28:03 +0300

    added second layer and more responsiveness

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,6 +3,9 @@ import './header.css'
 import background from '../../assets/bg.jpg'
 import smallTriangle from '../../assets/small-triangle.png'
 import pluses from '../../assets/pluses.png'
+import circle from '../../assets/circle.png'
+import pointingTriangleRight from '../../assets/pointing-triangle-right.png'
+import pointingTriangleLeft from '../../assets/pointing-triangle-left.png'
 
 
 const Header = () => {
@@ -15,8 +18,20 @@ const Header = () => {
         <img src={pluses}/>
       </div>
 
-      <div className='header-second-layer' id='layer'>
+      <div className='circle-container'>
+        <img src={circle} id='circle'/>
+        <img src={circle} id='circle'/>
+      </div>
 
+      <div className='header-second-layer' id='layer'>
+        <img src={pointingTriangleRight} id='pointing-triangle'/>
+        
+        <div className='second-layer-text'>
+          <h1 className='text-title'>DISCOVER</h1>
+          <p className='text-body'>DICK</p>
+        </div>
+
+        <img src={pointingTriangleLeft} id='pointing-triangle'/>
       </div>
     </div>
   )
